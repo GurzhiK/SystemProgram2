4db63f24ff263db5ea6daa9169338a7eb22abf6b
GurzhiK <94523039+GurzhiK@users.noreply.github.com>
2022-03-12 02:28:41 +0300

    Files

---
+++ send.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Задание 1</title>
+</head>
+<body>
+<b>Вы ввели:<b>
+    <br><br>
+<?php
+echo $_GET['text'];
+?>
+</body>
+</html>
